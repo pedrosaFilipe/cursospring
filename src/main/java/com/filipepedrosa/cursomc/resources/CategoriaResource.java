563d71c80e81f6d8b0b2cbe42d7f5e29cab13580
Filipe Pedrosa <parreiras.filipe@gmail.com>
2019-03-16 22:47:54 -0300

    Criando a classe de dominio Categoria

--- src/main/java/com/filipepedrosa/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/filipepedrosa/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package com.filipepedrosa.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.filipepedrosa.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping (value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest OK!";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> listaCategorias = new ArrayList<Categoria>();
+		listaCategorias.add(cat1);
+		listaCategorias.add(cat2);
+		
+		return listaCategorias;
 	}
 
 }
