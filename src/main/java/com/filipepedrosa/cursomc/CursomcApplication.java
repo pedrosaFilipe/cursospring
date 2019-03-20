a2fae15d4a6805929b08f6bcd9a08b9861e45a0b
Filipe Pedrosa <parreiras.filipe@gmail.com>
2019-03-19 23:25:20 -0300

    Correção do problema na implementação da relação entre Produto e Categoria

--- src/main/java/com/filipepedrosa/cursomc/CursomcApplication.java
+++ src/main/java/com/filipepedrosa/cursomc/CursomcApplication.java
@@ -40,8 +40,8 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 	}
 
